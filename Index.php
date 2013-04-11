213f35bbb3e53f98fbd0bba285a7d0a177379b5a
langdon2122 <langdon234@yahoo.com>
2013-04-11 22:29:52 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<h1> Operasi Matriks </h1>
+</head>
+<body>
+<button><a href="100.php">Matriks 100</a></button>
+<button><a href="90.php">Matriks 90</a></button>
+<button><a href="80.php">Matriks 80</a></button>
+<button><a href="70.php">Matriks 70</a></button>
+<button><a href="60.php">Matriks 60</a></button>
+<button><a href="50.php">Matriks 50</a></button>
+<button><a href="40.php">Matriks 40</a></button>
+<button><a href="30.php">Matriks 30</a></button>
+<button><a href="20.php">Matriks 20</a></button>
+<button><a href="10.php">Matriks 10</a></button>
+</body>
+</html>
